72d15eab43b0e9f4d3f702fb2014d2ed29bf67a0
Jaken Herman <JakenHerman7@Gmail.com>
2019-08-25 17:00:42 -0500

    completed section 2

--- 02/2.05.0/js/main.js
+++ 02/2.05.0/js/main.js
@@ -3,3 +3,16 @@
 *    Mastering Data Visualization with D3.js
 *    2.5 - Activity: Adding SVGs to the screen
 */
+
+const svg = d3.select('#chart-area').append('svg')
+  .attr('height', 500)
+  .attr('width', 500)
+  // to see the extra 100px of space we leave in rect width
+  .attr('style', 'background-color: yellow'); 
+
+const rectangle = svg.append('rect')
+  .attr('x', 0)
+  .attr('y', 0)
+  .attr('height', 500)
+  .attr('width', 400)
+  .attr('fill', 'purple');
--- 02/2.08.0/js/main.js
+++ 02/2.08.0/js/main.js
@@ -3,3 +3,32 @@
 *    Mastering Data Visualization with D3.js
 *    2.8 - Activity: Your first visualization!
 */
+
+d3.json('data/buildings.json').then((data) => {
+
+  data.forEach(element => {
+    element.height = +element.height;
+  });
+
+  const svg = d3.select('#chart-area').append('svg')
+    .attr('height', 500)
+    .attr('width', 500);
+
+  const bars = svg.selectAll('rect')
+    .data(data);
+
+  bars.enter()
+    .append('rect')
+    .attr('height', (d, i) => {
+      return d.height;
+    })
+    .attr('width', 25)
+    .attr('fill', 'orange')
+    .attr('x', (d, i) => {
+      return (i * 50) + 30;
+    })
+    .attr('y', 0);
+})
+.catch((err) => {
+  console.log(err);
+})
